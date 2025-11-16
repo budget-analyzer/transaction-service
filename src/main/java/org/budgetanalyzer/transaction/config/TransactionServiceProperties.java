c83d9ad334541322224434be692c796b1e6b671f
Bleu Rubin <bleurubin@gmail.com>
2025-11-16 07:30:57 -0500

    remove hyphen

--- src/main/java/org/budgetanalyzer/transaction/config/TransactionServiceProperties.java
+++ src/main/java/org/budgetanalyzer/transaction/config/TransactionServiceProperties.java
@@ -7,6 +7,6 @@ import jakarta.validation.Valid;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
-@ConfigurationProperties(prefix = "budget-analyzer.transaction-service")
+@ConfigurationProperties(prefix = "budgetanalyzer.transaction-service")
 @Validated
 public record TransactionServiceProperties(@Valid Map<String, CsvConfig> csvConfigMap) {}
